7216efbd3438effca10c9e27d64e0823c7bc4be5
mk <v.a.stekolschikov@gmail.com>
2022-11-18 15:44:20 +0200

    added path

--- frontend/src/pages/burgers/[id].tsx
+++ frontend/src/pages/burgers/[id].tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import styles from '../../styles/Burgers.module.css';
-import {IBurger} from "../burgers/index";
+import {IBurger} from "./type";
 
 export const getStaticPaths = async () => {
   const data = await fetch('http://localhost:5000/items')
@@ -24,23 +24,25 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps= async (context: { params: { id: any; }; }) => {
   const id = context.params.id;
-  const data = await fetch(`http://localhost:5000/items/${id}`)
+  const data = await fetch(`http://localhost:1337/api/burgers/${id}?populate=*`)
       .then( data => data.json())
       .catch(() => []);
+  const burger: IBurger = data.data
 
+  burger.attributes.image.data.attributes.url =  `http://localhost:1337${burger.attributes.image.data.attributes.url}`
   return {
-    props: { burger: data }
+    props: { burger }
   }
 }
 
 const Details: React.FC<{burger: IBurger}>  = ({ burger }) => {
   return (
        burger && <div className={styles.singleBurger}>
-      <h1>{ burger.name }</h1>
+      <h1>{ burger.attributes.name }</h1>
         <div className={styles.imageContainer}>
           <Image
-            src={`${burger.image}`}
-            alt={`${burger.name}`}
+            src={`${burger.attributes.image.data.attributes.url}`}
+            alt={`img`}
             width="100"
             height="100"
             layout="responsive"
@@ -48,7 +50,7 @@ const Details: React.FC<{burger: IBurger}>  = ({ burger }) => {
           />
         </div>
         <div>
-          <p>{ burger.desc }</p>
+          <p>{ burger.attributes.desc }</p>
       </div>
     </div>
   );
