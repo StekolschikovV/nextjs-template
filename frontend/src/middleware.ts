3339bda58d14ce64179ed179672117453d215388
mk <v.a.stekolschikov@gmail.com>
2022-11-21 21:06:17 +0200

    fixed lint

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,10 +2,11 @@ import type {NextFetchEvent, NextRequest} from 'next/server'
 import {NextResponse} from "next/server";
 
 export function middleware(request: NextRequest, event: NextFetchEvent) {
-    console.log("!!!", request.cookies.getAll())
+    // console.log("!!!", request.cookies.getAll())
     return NextResponse.next()
 }
 
 export const config = {
     matcher: ['/about', '/articles/:path*']
 }
+
